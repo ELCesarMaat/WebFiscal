a70c1450326d41ecb1d58628bd655db2bac7663d
ELCesarMaat <cesarmaat@hotmail.com>
2025-10-08 11:31:40 -0600

    add page icons

--- index.php
+++ index.php
@@ -18,6 +18,10 @@ $res = $conexion->query($sql);
 <!DOCTYPE html>
 <html lang="es">
 <head>
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo htmlspecialchars($empresaNombre); ?> | Temas fiscales y contables</title>
